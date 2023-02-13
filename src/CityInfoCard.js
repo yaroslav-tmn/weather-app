35a3140fdaae78c06aa0f4ca0cc577178a385806
Yaroslav Ostapenko <yaroslav.ostapenko@gmail.com>
2023-02-13 13:05:02 +0500

    Добавлены погодные условия

--- src/CityInfoCard.js
+++ src/CityInfoCard.js
@@ -1,10 +1,12 @@
 import "./CityInfoCard.css";
+import WeatherCodes from "./WeatherCodes";
 
 const CityInfoCard = (props) => {
   return (
     <div className="info-card">
       <h3>Город: {props.citydata.cityName}</h3>
       <h3>Температура: {props.citydata.cityTemp}</h3>
+      <h3>Погода: {WeatherCodes[props.citydata.cityWeatherCode]}</h3>
     </div>
   );
 };

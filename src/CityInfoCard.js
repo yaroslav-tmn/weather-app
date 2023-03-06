f7c6113ad6f1d3e96fd4f7b0a9071c6e1a408eba
Phaeton2040 <phaeton2040@gmail.com>
2023-03-06 18:09:52 +0300

    Fixes

--- src/CityInfoCard.js
+++ src/CityInfoCard.js
@@ -1,60 +1,65 @@
-import { useEffect, useRef, useState } from "react";
+import {useEffect, useRef, useState} from "react";
 import "./CityInfoCard.css";
 import WeatherCodes from "./WeatherCodes";
 
 const CityInfoCard = (props) => {
-  const placeImageRef = useRef();
-  const [placesWithImages, setPlacesWithImages] = useState();
+    const placeImageRef = useRef();
+    const [placesWithImages, setPlacesWithImages] = useState([]);
 
-  const getImagesUrls = async (ids) => {
-    const responses = await Promise.all(
-      ids.map((id) =>
-        fetch(
-          `http://api.opentripmap.com/0.1/ru/places/xid/${id}?apikey=5ae2e3f221c38a28845f05b67f614f533f4239a3fc9b5c8e16e194c9`
-        ).then((response) => response.json())
-      )
-    );
-    setPlacesWithImages(
-      responses.filter((obj) => obj.hasOwnProperty("preview"))
-    );
-    console.log(placesWithImages);
+    const getImagesUrls = async (ids) => {
+        const responses = await Promise.all(
+            ids.map((id) =>
+                fetch(
+                    `http://api.opentripmap.com/0.1/ru/places/xid/${id}?apikey=5ae2e3f221c38a28845f05b67f614f533f4239a3fc9b5c8e16e194c9`
+                ).then((response) => response.json())
+            )
+        );
+        setPlacesWithImages(
+            responses.filter((obj) => !!obj.preview)
+        );
+    };
 
-    if (placesWithImages.length > 0) {
-      let currentImage = 0;
-      const goSlideShow = () => {
-        placeImageRef.current.src =
-          placesWithImages[currentImage].preview.source;
-        currentImage++;
-        if (currentImage > placesWithImages.length) {
-          currentImage = 0;
+    useEffect(() => {
+        let interval;
+        if (placesWithImages.length > 0) {
+            let currentImage = 0;
+            const goSlideShow = () => {
+                placeImageRef.current.src =
+                    placesWithImages[currentImage].preview.source;
+                currentImage++;
+                if (currentImage >= placesWithImages.length) {
+                    currentImage = 0;
+                }
+            };
+            interval = setInterval(goSlideShow, 5000);
+        } else {
+            placeImageRef.current.src = "abstract_city.jpg";
         }
-      };
-      setInterval(goSlideShow, 5000);
-    } else {
-      placeImageRef.current.src = "abstract_city.jpg";
-    }
-  };
 
-  useEffect(() => {
-    getImagesUrls(props.places);
-  }, [props.places]);
+        return () => clearInterval(interval);
+    }, [placesWithImages]);
 
-  return (
-    <div className="info-card">
-      <span style={{ color: "#000000" }}>Город:</span> {props.citydata.cityName}
-      <br />
-      <span style={{ color: "#000000" }}>Температура:</span>
-      {props.citydata.cityTemp
-        ? ` ${Math.round(props.citydata.cityTemp)} °C`
-        : ""}
-      <br />
-      <span style={{ color: "#000000" }}>Погода:</span>{" "}
-      {WeatherCodes[props.citydata.cityWeatherCode]}
-      <br />
-      <br />
-      <img src="abstract_city.jpg" alt="Place" ref={placeImageRef} />
-    </div>
-  );
+    useEffect(() => {
+        setPlacesWithImages([]);
+        getImagesUrls(props.places);
+    }, [props.places]);
+
+    return (
+        <div className="info-card">
+            <span style={{color: "#000000"}}>Город:</span> {props.citydata.cityName}
+            <br/>
+            <span style={{color: "#000000"}}>Температура:</span>
+            {props.citydata.cityTemp
+                ? ` ${Math.round(props.citydata.cityTemp)} °C`
+                : ""}
+            <br/>
+            <span style={{color: "#000000"}}>Погода:</span>{" "}
+            {WeatherCodes[props.citydata.cityWeatherCode]}
+            <br/>
+            <br/>
+            <img src="abstract_city.jpg" alt="Place" ref={placeImageRef}/>
+        </div>
+    );
 };
 
 export default CityInfoCard;

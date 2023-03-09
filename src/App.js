a801e73d84b61f1636142802ce62e0ae816ab379
Yaroslav Ostapenko <yaroslav.ostapenko@gmail.com>
2023-03-09 23:35:19 +0500

    Final design 1.1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
 import CitySelector from "./CitySelector";
+import "./CitySelector.css";
 
 function App() {
   return (
     <div className="app">
       <CitySelector />
+      <p className="city-selector">
+        Это приложение показывает текущую погоду в выбранном городе, а так же
+        отображает интересные изображения, связанные с городом, если удается их
+        получить.
+      </p>
     </div>
   );
 }
